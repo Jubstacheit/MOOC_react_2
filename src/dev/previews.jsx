1f965208b6ccab358f42a4593c8dc26850f3b2a3
Viviane <dessaintviviane@gmail.com>
2023-06-09 00:42:30 +0200

    Commit chapter 5

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
 import {PaletteTree} from './palette'
 import App from "../App";
+import UserList from "../pages/UserList";
+import Register from "../pages/Register";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
             <ComponentPreview path="/App">
                 <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/UserList">
+                <UserList/>
+            </ComponentPreview>
+            <ComponentPreview path="/Register">
+                <Register/>
             </ComponentPreview>
         </Previews>
     )
